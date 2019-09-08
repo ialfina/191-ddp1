12f086d01b6f198d3837900b574ece1450618fa8
Ika Alfina <ika.alfina@gmail.com>
2019-09-08 11:19:15 +0700

    add hello.py

---
+++ ch01/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 10:51:49 2019
+
+@author: ialfina
+"""
+
+firstName = input("First name: ")
+lastName = input("Last name: ")
+
+print("Hello,", firstName, lastName, "!")
+print("Hello, {} {}!".format(firstName, lastName))
+print("Hello, " + firstName + " " + lastName + "!")
